c6a0cdff326620b36d955686969955df8f7705ce
mio <qwe@qwe.com>
2021-11-15 03:03:46 +0700

    init

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -7,16 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText input_text;
+    private EditText text_out;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         input_text = findViewById(R.id.input);
+        text_out = findViewById(R.id.editText1);
 
+        if (getIntent().getStringExtra("text_out") != null){
+            text_out.setText((getIntent().getStringExtra("text_out")));
+        }
     }
 
     public void sendData(View view){
--- app/src/main/java/com/example/lab1/NewActivity.java
+++ app/src/main/java/com/example/lab1/NewActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +13,12 @@ import androidx.annotation.Nullable;
 
 public class NewActivity extends Activity {
 
-
+    private EditText set_text;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
-
+        set_text =  findViewById(R.id.set_text);
         TextView text_obj = findViewById(R.id.text);
 
         text_obj.setText((getIntent().getStringExtra("text")));
@@ -25,6 +26,8 @@ public class NewActivity extends Activity {
 
     public void back(View view){
         Intent intent = new Intent(this, MainActivity.class);
+
+        intent.putExtra("text_out", set_text.getText().toString());
         startActivity(intent);
     }
 }
